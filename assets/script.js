b129e839c69cf197e775cae149d19c2a256fe183
ziad faih <ziad.faih@gmail.com>
2022-11-23 17:21:16 +0100

    new

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//récupération des données pour les mettre dans le panier
+//partie back end
+
+
+//récuperer les éléments
+
+let quantity = document.querySelector("#input");
+
+
+
+
+
+// Récupération au début de la page du localStorage "LSproduct"
+let LSProducts = JSON.parse(localStorage.getItem("LSproducts"));
